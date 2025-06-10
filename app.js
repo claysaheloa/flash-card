f51445da0735b229e20b860e0b2a0e7c95d34247
claysaheloa <claysa.franca@escola.pr.gov.br>
2025-06-10 14:28:17 +0000

    v

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ let container = document.getElementById ('container')
 let cartao = document.getElemented ('artifice')
 cartao.className= 'cartao'
 cartao.innerHTML= 
+<article class=  {
+    constructor(parameters) {
+        
+    }
+}
 }
